25fe77d49a3858b40a6be694c4b67afe2d81bdcf
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-10-11 00:46:17 -0500

    Update p5.h

--- p5.h
+++ p5.h
@@ -4,5 +4,9 @@
 
 #ifndef TAREA_3_LIBRERIAS_P5_H
 #define TAREA_3_LIBRERIAS_P5_H
-
+#include <iostream>
+#include <tuple>
+using namespace std;
+template <class... T>
+tuple<T&...> unpack(T&...args){return{args...};}
 #endif //TAREA_3_LIBRERIAS_P5_H
