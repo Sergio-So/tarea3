daf8a76e020874310ed20dd32397562bf2e4c946
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-10-11 22:39:51 -0500

    Update p1.h

--- p1.h
+++ p1.h
@@ -17,6 +17,6 @@ vector<vector<int>> split_range(T v1, int n){
     for (auto && l:v1){
       v2.push_back(l);c++;
       if(c == ce){m.push_back(v2);v2.erase(v2.begin(), v2.end());c=0;}}
-    for (int i=0;i<v2.size();i++){m[n -1].push_back(v2[i]);}
+    for (int i=0;i<v2.size();i++){m[n-1].push_back(v2[i]);}
     return m;}
 #endif //TAREA_3_LIBRERIAS_P1_H
