d3915ed1360306343e8a6b785059664194ebcaf9
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-10-11 22:33:00 -0500

    Update p8.h

--- p8.h
+++ p8.h
@@ -4,5 +4,18 @@
 
 #ifndef TAREA_3_LIBRERIAS_P8_H
 #define TAREA_3_LIBRERIAS_P8_H
+#include <iostream>
+#include <forward_list>
+#include <fstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <deque>
+using namespace std;
+template <template<class> class Container=vector, class T>
+Container<T> filter_codes(T file, char character){
+    ifstream document(file);string ch;Container<T> out;
+    if (document.is_open()){while (getline(document, ch)){if (ch[0] == character){out.push_back(ch);}}}
+    document.close();sort(out.begin(), out.end());return out;}
 
 #endif //TAREA_3_LIBRERIAS_P8_H
